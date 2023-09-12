b5a0fb5fa91c00c3cadb78f6b48f8f57a974524d
Ana-BSL <107702462+Ana-BSL@users.noreply.github.com>
2023-09-12 17:45:17 -0300

    Create MemoriaFisica.java

---
+++ MemoriaFisica.java
@@ -0,0 +1,52 @@
+public class MemoriaFisica {
+    String[] espaco = new String[8];
+    Disco disco; // Referência para a classe Disco
+    MemoriaVirtual memoriaVirtual; // Referência para a classe MemoriaVirtual
+
+    public MemoriaFisica(Disco disco, MemoriaVirtual memoriaVirtual) {
+        this.disco = disco;
+        this.memoriaVirtual = memoriaVirtual;
+    }
+
+    public void pegarDados() {
+        if (cheia()) {
+            moverParaDisco();
+        }
+
+        int posicaoVazia = encontrarPosicaoVazia();
+        if (posicaoVazia != -1) {
+            String dado = memoriaVirtual.preencheMvirtual();
+            espaco[posicaoVazia] = dado;
+        }
+    }
+
+    private boolean cheia() {
+        for (String elemento : espaco) {
+            if (elemento == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int encontrarPosicaoVazia() {
+        for (int i = 0; i < espaco.length; i++) {
+            if (espaco[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void moverParaDisco() {
+        if (disco != null && espaco[0] != null) {
+            disco.preencheDisco(espaco[0]);
+            // Remova o elemento da primeira posição após armazená-lo no disco
+            for (int i = 0; i < espaco.length - 1; i++) {
+                espaco[i] = espaco[i + 1];
+            }
+            espaco[espaco.length - 1] = null;
+        }
+    }
+}
+
